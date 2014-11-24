047b74edfbee07318ed5d3b42c85cf70031d95ae
Marc Stroebel <marc.stroebel@24.com>
2014-11-24 09:59:09 +0200

    Initial commit of project (no examples yet). Just includes the Extension class itself.

---
+++ UIView-Blur/UIView-BlurTests/UIView_BlurTests.swift
@@ -0,0 +1,36 @@
+//
+//  UIView_BlurTests.swift
+//  UIView-BlurTests
+//
+//  Created by Marc Stroebel on 2014/11/24.
+//  Copyright (c) 2014 StroebelSoftware. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class UIView_BlurTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
